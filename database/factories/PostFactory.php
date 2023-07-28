6d428c0bd3f3b6fdd2a9969a262139455c8f0da6
Adi Putra <m.a.syaputra.94@gmail.com>
2023-07-28 09:57:07 +0700

    ptogress 15%

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->title(),
+            'category_id' => function ()  {
+                return Category::all()->random();
+            },
+
+        ];
+    }
+}
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -10,7 +10,16 @@
     </li>
 
     <li class="nav-item mt-3">
-        <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+        <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Post</h6>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="/">
+            <div
+                class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Category & Tag</span>
+        </a>
     </li>
 
 </ul>
